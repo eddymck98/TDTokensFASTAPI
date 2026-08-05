1f98cda1727bdace0e77ba68cc9e8317e4d39922
eddymck98 <eddymck98@gmail.com>
2026-08-05 23:05:50 +0100

    Update profile.py

--- routers/profile.py
+++ routers/profile.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends, status
+from fastapi import APIRouter, Request, Form, Depends, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from utils.database import get_supabase_client
